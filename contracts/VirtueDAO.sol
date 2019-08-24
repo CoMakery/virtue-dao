9b6700892c4dd4b8f2b547d9614b8c7a54fca6ef
Noah Thorp <noah@comakery.com>
2019-08-24 20:28:36 +0200

    minor refactoring

--- contracts/VirtueDAO.sol
+++ contracts/VirtueDAO.sol
@@ -4,26 +4,13 @@ contract VirtueDAO {
     // TODO: add SafeMath back after sketch phase
     // using SafeMath for uint;
 
-    // mapping(uint => mapping(address => uint)) public virtueBalances; // virtuid => holderAddress => balance
-    
-    // struct MemberVirtue {
-    //     uint[5] virtueBalances; // TODO: make this flexible
-    //     uint totalVirtue;
-    // }
-
-    mapping(address => mapping(uint => uint)) public memberVirtues; // memberAddress => virtueType => balance
+    mapping(address => mapping(uint => uint)) public memberVirtues; // memberAddress => virtueId => balance
     mapping(uint => mapping(address => uint)) public awardsMadeThisPeriod; // periodId => user => awardsMadeThisPeriod
     
     // token awards
     uint public maxVirtueId = 5;
     uint public maxAwardablePerPeriod = 100;
     uint public currentPeriod = 0;
-    
-    // returns total funds available from patrons 
-    // function endowmentBalance() public returns(uint) {
-    //     // TODO: call external DAI address for balance
-    //     return 0;
-    // }
     
     function getAwardsMadeThisPeriod(address _member) public view returns (uint) {
         return awardsMadeThisPeriod[currentPeriod][_member];
@@ -33,18 +20,25 @@ contract VirtueDAO {
         return maxAwardablePerPeriod - getAwardsMadeThisPeriod(_member);
     }
     
-    function awardVirtue(address _member, uint _virtueType, uint amount) public returns (uint) {
-        return memberVirtues[_member][_virtueType] += amount;
+    function awardVirtue(address _member, uint _virtueId, uint amount) public returns (uint) {
+        return memberVirtues[_member][_virtueId] += amount;
     }
     
     function virtueCount() public view returns (uint) {
         return maxVirtueId + 1;
     }
     
-    function getVirtue(address _member, uint _virtueType) public view returns (uint) {
-        return memberVirtues[_member][_virtueType];
+    function getVirtue(address _member, uint _virtueId) public view returns (uint) {
+        return memberVirtues[_member][_virtueId];
     }
+
+    // TODO: virtue CAP / max dividend 
+    // TODO: these functions
     
+    // function nextPeriod()
+
+    // function decayVirtue()
+
     // // Virtue -> rDAI: transferFrom(daoAddress, daoMemberAddress, amount)
     // // Member1 -> DAI: transfer(recipientAddress, amount) \n// DAI is member'
     // function claimFellowship() public {
@@ -56,5 +50,11 @@ contract VirtueDAO {
     // // endowment available during this period
     // function _calculateFellowship(address _member) private {
     //     // stub
+    // }
+
+    // returns total funds available from patrons 
+    // function endowmentBalance() public returns(uint) {
+    //     // TODO: call external DAI address for balance
+    //     return 0;
     // }
 }
