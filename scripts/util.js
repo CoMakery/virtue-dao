c1f66e794c71bd65ce3622436c1eab4534583a96
Noah Thorp <noah@comakery.com>
2019-08-25 09:14:23 +0200

    send awards and update points

--- scripts/util.js
+++ scripts/util.js
@@ -6,3 +6,28 @@ window.util.getABI = async function getABI(path) {
     })
     return rawResponse["abi"];
 }
+
+window.util.pollForCompletion = function pollForCompletion(txHash, callback) {
+    let calledBack = false
+    const checkInterval = setInterval(function () {
+        const notYet = 'response has no error or result'
+        ethereum.sendAsync({
+            method: 'eth_getTransactionByHash',
+            params: [txHash],
+        }, function (err, response) {
+            if (calledBack) return
+            if (err || response.error) {
+                if (err.message.includes(notYet)) {
+                    return 'transactiion is not yet mined'
+                }
+
+                callback(err || response.error)
+            }
+
+            const transaction = response.result
+            clearInterval(checkInterval)
+            calledBack = true
+            callback(null, transaction)
+        })
+    }, 2000)
+}
