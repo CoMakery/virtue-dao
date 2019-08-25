9f0c90d9e53af0650e030e482ce152c4d3e9a16a
Noah Thorp <noah@comakery.com>
2019-08-25 06:57:18 +0200

    configure node.js with browserify

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,19 @@
+window.Web3 = require('web3')
 console.log("hi from main.js")
+
+async function run() {
+    let ethereum = window.ethereum;
+    let web3 = window.web3;
+    if (typeof ethereum !== 'undefined') {
+        await ethereum.enable();
+        web3 = new Web3(ethereum);
+    } else if (typeof web3 !== 'undefined') {
+        web3 = new Web3(web3.currentProvider);
+    } else {
+        web3 = new Web3(new Web3.providers.HttpProvider(process.env.WEB3_PROVIDER));
+    }
+    let addr = await web3.eth.getCoinbase()
+    console.log(addr)
+}
+
+document.addEventListener('DOMContentLoaded', run)
